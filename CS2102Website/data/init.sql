67b46057101cf2a3e9d7cb8dce6bf417540f226c
Aerin <aerinnnnn@gmail.com>
2020-11-02 21:01:42 +0800

    edited init.sql users table

--- CS2102Website/data/init.sql
+++ CS2102Website/data/init.sql
@@ -15,10 +15,10 @@ DROP TABLE IF EXISTS users;
 -- initialisation of database tables
 CREATE TABLE users (
   username VARCHAR(255) PRIMARY KEY,
-  contact_num CHAR(8),
-  password VARCHAR(255),
-  name VARCHAR(255),
-  email VARCHAR(255)
+  contact_num CHAR(8) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL UNIQUE
 );
 
 CREATE TABLE pet_owner (
