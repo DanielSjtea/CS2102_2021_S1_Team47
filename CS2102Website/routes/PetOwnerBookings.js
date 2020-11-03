e776a58a48802bf4af78a843b84d21f8140c8d9c
alyssanxt <e0417807@u.nus.edu>
2020-11-03 18:14:39 +0800

    edited bidsSubmitted, viewBidsSubmitted

--- CS2102Website/routes/PetOwnerBookings.js
+++ CS2102Website/routes/PetOwnerBookings.js
@@ -6,7 +6,34 @@ var router = express.Router();
 var sql = require("../data/queries");
 
 router.get("/", function(req, res, next) {
-    res.render("PetOwnerBookings");
+//    res.render("PetOwnerBookings");
+        var user = req.user;
+        database.query(sql.successful_bids_made_as_petowner, [user.username], (err, data) => {
+            if(err) {
+                console.log("SQL error: " + err);
+            } else {
+                if(data.rowCount > 0) {
+//                    var contact = null;
+//                    var name = null;
+//                    var pType = null;
+                    var ctUsername = data.rows[0].care_taker_username;
+//                    database.query(sql.get_caretaker_profile, [ctUsername], (err, ctData) => {
+//                        contact = ctData.rows[0].contact_num;
+//                        name = ctData.rows[0].name;
+//                    });
+//                    database.query(sql.get_ct_pet_types, [ctUsername], (err, pListData) => {
+//                        pType = pListData.rows[0].ptype;
+//                    });
+                    var sDate = data.rows[0].s_date;
+                    var returnDate = data.rows[0].care_taker_username;
+
+                    console.log("pType is " + sDate);
+
+                    res.render("PetOwnerBookings");
+                }
+            }
+        });
+//        res.render("PetOwnerBookings");
 });
 
 module.exports = router;
--- CS2102Website/routes/viewSubmittedBid.js
+++ CS2102Website/routes/viewSubmittedBid.js
@@ -6,7 +6,41 @@ var router = express.Router();
 var sql = require("../data/queries");
 
 router.get("/", function(req, res, next) {
-    res.render("viewSubmittedBid");
+//    res.render("viewSubmittedBid");
+        // hsould be under view submitted bid
+        var user = req.user;
+        database.query(sql.successful_bids_made_as_petowner, [user.username], (err, data) => {
+            if(err) {
+                console.log("SQL error: " + err);
+            } else {
+                if(data.rowCount > 0) {
+                    var ctusername = data.rows[0].care_taker_username;
+                    var startDate = data.rows[0].s_date;
+                    var startTime = data.rows[0].s_time;
+                    var endTime = data.rows[0].e_time;
+                    var petName = data.rows[0].name;
+                    var review = data.rows[0].review;
+                    var trfMethod = data.rows[0].trf_mthd;
+                    var payType = data.rows[0].pay_type;
+                    var rating = data.rows[0].rating;
+                    var svcType = data.rows[0].svc_type;
+                    var bid = data.rows[0].price;
+                    res.render("viewSubmittedBid",{
+                        ctusername: ctusername,
+                        startDate: startDate,
+                        startTime: startTime,
+                        endTime: endTime,
+                        petName: petName,
+                        review: review,
+                        payType:payType,
+                        rating: rating,
+                        svcType: svcType,
+                        trfMethod: trfMethod,
+                        bid: bid
+                     });
+                }
+            }
+        });
 });
 
 module.exports = router;
