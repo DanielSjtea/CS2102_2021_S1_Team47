004fdd5f35510dbb75ea073976f9495fa21e075c
cheongisabella <cheongisabella@gmail.com>
2020-11-03 23:45:13 +0800

    Edit viewSitterProfile reviews

--- CS2102Website/routes/viewSitterProfile.js
+++ CS2102Website/routes/viewSitterProfile.js
@@ -8,34 +8,41 @@ var sql = require("../data/queries");
 router.get('/', async function(req, res, next) {
     let username = req.session.caretakerUsername;
 
-    /*var caretaker = await database.db_get_promise(sql.get_caretaker_profile, [username]);
-    var past_reviews = await database.db_get_promise(sql.view_caretaker_review, [username]); 
-    var review_count = Math.min(past_reviews.length, 3);
-    res.render("viewSitterProfile", {
-        caretaker: caretaker,
-        past_reviews: past_reviews,
-        review_count: review_count*/
-
-    database.query(sql.get_caretaker_profile, [username], (err, data) => {
+    /*database.query(sql.get_caretaker_profile, [username], (err, data) => {
       if (err) {
         console.log("SQL Error: " + err);
       } else {
         var caretaker;
-        var past_reviews = await database.db_get_promise(sql.view_caretaker_review, [username]); 
-        var review_count = Math.min(past_reviews.length, 3);
+        var past_reviews;
+        var review_count;
         if (data.rowCount > 1) {
           caretaker = data.rows[0];
         } else {
           caretaker = data.rows;
         }
         res.render("viewSitterProfile", {
-          caretaker: caretaker
-          past_reviews: past_reviews,
-          review_count: review_count
+              caretaker: caretaker,
+              past_reviews: past_reviews,
+              review_count: review_count
         });
       }
-    });
+    });*/
+
+    var caretaker = await database.db_get_promise(sql.get_caretaker_profile, [username]);
+    var past_reviews = await database.db_get_promise(sql.view_caretaker_review, [username]); 
+    var review_count = Math.min(past_reviews.length, 3);
+    res.render("viewSitterProfile", {
+        caretaker: caretaker,
+        past_reviews: past_reviews,
+        review_count: review_count
+    }); 
+
 });
+
+
+
+
+
 
 // POST
 router.post('/', function(req, res, next) {    
