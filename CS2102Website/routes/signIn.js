eddba89237a6983bd39e2a017b025d12709e5380
marcusng98 <marcusngcheeheng@gmail.com>
2020-11-06 02:35:28 +0800

    Changed bookings page css

--- CS2102Website/routes/signIn.js
+++ CS2102Website/routes/signIn.js
@@ -14,15 +14,15 @@ router.post("/", function(req, res, next) {
     var role = req.body.role;
     console.log(role)
     passport.authenticate('local', function(err, user, info) {
-        if (err) { 
-            return next(err); 
+        if (err) {
+            return next(err);
         }
-        if (!user) { 
+        if (!user) {
             return res.redirect("signIn", {message: "An error has occured while signing in. Please try again!"}); 
         }
         req.logIn(user, async function(err) {
-            if (err) { 
-                return next(err); 
+            if (err) {
+                return next(err);
             }
             if (role != 'Role') {
                 if (role == "Pet Owner/Care Taker") {
