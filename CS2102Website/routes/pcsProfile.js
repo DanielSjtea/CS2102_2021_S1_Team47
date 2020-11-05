76eeb177e9a6972465b2cda36a5eb8fa68f7652e
cheongisabella <cheongisabella@gmail.com>
2020-11-05 19:31:27 +0800

    editPCSProfile

--- CS2102Website/routes/pcsProfile.js
+++ CS2102Website/routes/pcsProfile.js
@@ -8,20 +8,28 @@ var sql = require("../data/queries");
 
 router.get("/", function(req, res, next) {
     var user = req.user;
-            database.query(sql.is_admin, [user.username], (err, data) => {
-                if(err) {
-                    console.log("SQL error: " + err);
-                } else {
-                    if(data.rowCount > 0) {
-                        res.render("pcsProfile", {
-                            name: user.name,
-                            email: user.email,
-                            username: user.username,
-                            contact_num: user.contactNum,
-                        });
+    database.query(sql.is_admin, [user.username], (err, data) => {
+        if(err) {
+            console.log("SQL error: " + err);
+        } else {
+            if(data.rowCount > 0) {
+                database.query(sql.get_profile, [user.username], (err, data) => {
+                    if(err) {
+                        console.log("SQL error: " + err);
+                    } else {
+                        if(data.rowCount > 0) {
+                            res.render("pcsProfile", {
+                                name: data.rows[0].name,
+                                email: data.rows[0].email,
+                                username: data.rows[0].username,
+                                contact_num: data.rows[0].contact_num,
+                            });
+                        }
                     }
-                }
-            });
+                });
+            }
+        }
+    });
 });
 
 module.exports = router;
