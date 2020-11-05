deb18d0b6dfb269be489067895d8da334f2630e4
alyssanxt <e0417807@u.nus.edu>
2020-11-05 19:04:10 +0800

    edited bidsSubmitted and myBids

--- CS2102Website/routes/bidsSubmitted.js
+++ CS2102Website/routes/bidsSubmitted.js
@@ -11,19 +11,24 @@ router.get("/", function(req, res, next) {
         var ctusername = null;
         database.query(sql.successful_bids_made_as_petowner, [user.username], (err, data) => {
         if (data.rowCount > 0){
-                    var name = null;
-                    ctusername = data.rows[0].care_taker_username;
-                    var startDate = data.rows[0].s_date;
-                    var bid = data.rows[0].price;
-                    database.query(sql.get_caretaker_profile, [ctusername], (err, ctdata) => {
-                        name = ctdata.rows[0].name;
-                        res.render("bidsSubmitted",{
-                            ctusername: ctusername,
-                            startDate: startDate,
-                            bid: bid,
-                            name: name
-                        });
-                    });
+            var bidArr = null;
+                res.render("bidsSubmitted",{
+                    ctusername: user.username,
+                    bidArr: data.rows
+                });
+//                    var name = null;
+//                    ctusername = data.rows[0].care_taker_username;
+//                    var startDate = data.rows[0].s_date;
+//                    var bid = data.rows[0].price;
+//                    database.query(sql.get_caretaker_profile, [ctusername], (err, ctdata) => {
+//                        name = ctdata.rows[0].name;
+//                        res.render("bidsSubmitted",{
+//                            ctusername: ctusername,
+//                            startDate: startDate,
+//                            bid: bid,
+//                            name: name
+//                        });
+//                    });
         } else {
             res.render("bidsSubmitted", {
                 ctusername: null
