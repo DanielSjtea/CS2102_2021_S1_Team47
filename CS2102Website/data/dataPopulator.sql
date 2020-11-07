c7dff381f92c1a2bc53200af70e9438415ad01f8
alyssanxt <54824858+alyssanxt@users.noreply.github.com>
2020-11-07 15:36:50 +0800

    Update dataPopulator.sql

--- CS2102Website/data/dataPopulator.sql
+++ CS2102Website/data/dataPopulator.sql
@@ -18,6 +18,13 @@ insert into users (username, contact_num, password, name, email) values ('lloadw
 insert into users (username, contact_num, password, name, email) values ('jmaddysonh', '64654243', '$2b$10$nD3nK6.VrfGkZXwcqm/uGO50qbaNG69fsvekjOf./85vkmn.o5fRi', 'Joana Maddyson', 'jmaddysonh@privacy.gov.au');
 insert into users (username, contact_num, password, name, email) values ('gniavei', '98748422', '$2b$10$nD3nK6.VrfGkZXwcqm/uGO50qbaNG69fsvekjOf./85vkmn.o5fRi', 'Gardy Niave', 'gniavei@japanpost.jp');
 insert into users (username, contact_num, password, name, email) values ('aachesonj', '98092940', '$2b$10$nD3nK6.VrfGkZXwcqm/uGO50qbaNG69fsvekjOf./85vkmn.o5fRi', 'Amory Acheson', 'aachesonj@indiegogo.com');
+insert into users (username, contact_num, password, name, email) values ('sollicitudin', '56535746', '$2b$10$nD3nK6.VrfGkZXwcqm/uGO50qbaNG69fsvekjOf./85vkmn.o5fRi', 'Curran', 'augue@nullaDonec.co.uk');
+insert into users (username, contact_num, password, name, email) values ('accumsan', '85980973', '$2b$10$nD3nK6.VrfGkZXwcqm/uGO50qbaNG69fsvekjOf./85vkmn.o5fRi', 'Amethyst', 'Vestibulum.ante@id.net');
+insert into users (username, contact_num, password, name, email) values ('lacinia' , '16093392', '$2b$10$nD3nK6.VrfGkZXwcqm/uGO50qbaNG69fsvekjOf./85vkmn.o5fRi', 'Travis', 'odio.semper@magnaDuisdignissim.org');
+insert into users (username, contact_num, password, name, email) values ('dignissim', '78295090', '$2b$10$nD3nK6.VrfGkZXwcqm/uGO50qbaNG69fsvekjOf./85vkmn.o5fRi', 'Elton', 'iaculis.quis.pede@aliquamenim.ca');
+insert into users (username, contact_num, password, name, email) values ('praesen' , '21443907', '$2b$10$nD3nK6.VrfGkZXwcqm/uGO50qbaNG69fsvekjOf./85vkmn.o5fRi', 'Amir', 'dapibus.rutrum@venenatis.net');
+
+
 
 /* PCS Admin Users*/
 insert into users (username, contact_num, password, name, email) values ('tbryan9', '91100539', '$2b$10$nD3nK6.VrfGkZXwcqm/uGO50qbaNG69fsvekjOf./85vkmn.o5fRi', 'Tomasina Bryan', 'tbryan9@state.gov');
@@ -30,6 +37,13 @@ insert into users (username, contact_num, password, name, email) values ('fpalum
 insert into users (username, contact_num, password, name, email) values ('chanfreyg', '97925679', '$2b$10$nD3nK6.VrfGkZXwcqm/uGO50qbaNG69fsvekjOf./85vkmn.o5fRi', 'Carline Hanfrey', 'chanfreyg@mapy.cz');
 insert into users (username, contact_num, password, name, email) values ('pbrahanh', '90497815', '$2b$10$nD3nK6.VrfGkZXwcqm/uGO50qbaNG69fsvekjOf./85vkmn.o5fRi', 'Patty Brahan', 'pbrahanh@trellian.com');
 insert into users (username, contact_num, password, name, email) values ('pgornari', '94337642', '$2b$10$nD3nK6.VrfGkZXwcqm/uGO50qbaNG69fsvekjOf./85vkmn.o5fRi', 'Philippine Gornar', 'pgornari@seesaa.net');
+insert into users (username, contact_num, password, name, email) values ('ornare', '91100539', '$2b$10$nD3nK6.VrfGkZXwcqm/uGO50qbaNG69fsvekjOf./85vkmn.o5fRi', 'Ornare', 'o@state.gov');
+insert into users (username, contact_num, password, name, email) values ('ligula', '91100539', '$2b$10$nD3nK6.VrfGkZXwcqm/uGO50qbaNG69fsvekjOf./85vkmn.o5fRi', 'ligula', 'll@state.gov');
+insert into users (username, contact_num, password, name, email) values ('sociosqu', '91100539', '$2b$10$nD3nK6.VrfGkZXwcqm/uGO50qbaNG69fsvekjOf./85vkmn.o5fRi', 'sociosqu', 'sociosqu@state.gov');
+insert into users (username, contact_num, password, name, email) values ('blandit', '91100539', '$2b$10$nD3nK6.VrfGkZXwcqm/uGO50qbaNG69fsvekjOf./85vkmn.o5fRi', 'blandit', 'bland@state.gov');
+insert into users (username, contact_num, password, name, email) values ('facilisis', '91100539', '$2b$10$nD3nK6.VrfGkZXwcqm/uGO50qbaNG69fsvekjOf./85vkmn.o5fRi', 'facilisis', 'fff@state.gov');
+
+
 
 insert into pet_owner (username, card_cvc, card_name, card_no, card_brand, area) values ('deverix0', '314', 'Donn Everix', '561013598073204074', 'china-unionpay', 'Bishan');
 insert into pet_owner (username, card_cvc, card_name, card_no, card_brand, area) values ('emacneilage1', '497', 'Erica MacNeilage', '3580093464312583', 'jcb', 'Ang Mo Kio');
@@ -53,6 +67,13 @@ insert into pet_owner (username, card_cvc, card_name, card_no, card_brand, area)
 insert into pet_owner (username, card_cvc, card_name, card_no, card_brand, area) values ('gniavei', '965', 'Gardy Niave', '3572529233697341', 'jcb', 'Ang Mo Kio');
 insert into pet_owner (username, card_cvc, card_name, card_no, card_brand, area) values ('aachesonj', '834', 'Amory Acheson', '5602256600683964', 'bankcard', 'Bedok');
 
+insert into pet_owner (username, card_cvc, card_name, card_no, card_brand, area) values ('sollicitudin', '188', 'Curran', '8473202436993158', 'jcb', 'Ang Mo Kio');
+insert into pet_owner (username, card_cvc, card_name, card_no, card_brand, area) values ('accumsan', '234', 'Amethyst', '9173949656024140', 'visa-electron', 'Bedok');
+insert into pet_owner (username, card_cvc, card_name, card_no, card_brand, area) values ('lacinia' , '767' , 'Travis', '3758661669831373', 'jcb', 'Tampines');
+insert into pet_owner (username, card_cvc, card_name, card_no, card_brand, area) values ('praesen' , '667', 'Amir',  '2944268646280201', 'jcb', 'Bishan');
+insert into pet_owner (username, card_cvc, card_name, card_no, card_brand, area) values ('dignissim', '098', 'Elton', '4414282914434328', 'diners-club-carte-blanche', 'Bedok');
+
+
 INSERT INTO care_taker (username, ctype, area) VALUES 
 ('acallera', 'Full Time','Tampines'),
 ('dfridayb', 'Full Time','Serangoon'),
@@ -63,9 +84,16 @@ INSERT INTO care_taker (username, ctype, area) VALUES
 ('lloadwickg', 'Part Time','Yishun'),
 ('jmaddysonh', 'Part Time','Jurong'),
 ('gniavei', 'Part Time','Ang Mo Kio'),
-('aachesonj', 'Part Time','Bedok');
+('aachesonj', 'Part Time','Bedok'),
+('sollicitudin','Full Time','Serangoon'),
+('accumsan', 'Part Time','Yishun'),
+('lacinia', 'Part Time','Yishun'),
+('dignissim', 'Part Time','Jurong'),
+('praesen', 'Part Time','Ang Mo Kio');
 
-insert into pcs_admin (username) values ('tbryan9'), ('agloyensa'), ('miannellob'), ('wrosemanc'), ('hmayzed'), ('srummerye'), ('fpalumbof'), ('chanfreyg'), ('pbrahanh'), ('pgornari');
+
+
+insert into pcs_admin (username) values ('ornare'), ('ligula'), ('sociosqu'), ('blandit'), ('facilisis'),('tbryan9'), ('agloyensa'), ('miannellob'), ('wrosemanc'), ('hmayzed'), ('srummerye'), ('fpalumbof'), ('chanfreyg'), ('pbrahanh'), ('pgornari');
 
 INSERT INTO owns_pet (pet_owner_username, name, ptype, sp_req) VALUES
 ('deverix0', 'Milo', 'Dog', 'None'),
@@ -94,7 +122,13 @@ INSERT INTO specify_trf_pref (care_taker_username, trf_mthd) VALUES
 ('lloadwickg', 'Cycle'),
 ('jmaddysonh', 'Car'),
 ('gniavei', 'Car'),
-('aachesonj', 'Car');
+('aachesonj', 'Car'),
+('sollicitudin', 'Bike'),
+('accumsan', 'Car'),
+('lacinia', 'Bike'),
+('dignissim', 'Car'),
+('praesen', 'Bike');
+
 
 
 /* TO BE CONFIRMED */
@@ -108,7 +142,13 @@ INSERT INTO pay (care_taker_username, pcs_admin_username, bonus, date) VALUES
 ('lloadwickg', 'chanfreyg', 0, '2020-10-30'),
 ('jmaddysonh', 'pbrahanh', 0, '2020-10-30'),
 ('gniavei', 'pgornari', 0, '2020-10-30'),
-('aachesonj', 'wrosemanc', 0, '2020-10-30');
+('aachesonj', 'wrosemanc', 0, '2020-10-30'),
+('sollicitudin', 'ornare',10, '2020-12-11'),
+('accumsan', 'ligula', 10, '2020-12-12'),
+('lacinia', 'sociosqu', 10, '2020-12-13'),
+('dignissim', 'blandit', 10, '2020-12-14'),
+('praesen', 'facilisis',10, '2020-12-15');
+
 
 INSERT INTO has_price_list (care_taker_username, ptype, price) VALUES 
 ('acallera', 'Dog', 5),
@@ -123,7 +163,15 @@ INSERT INTO has_price_list (care_taker_username, ptype, price) VALUES
 ('lloadwickg', 'Cat', 5),
 ('jmaddysonh', 'Cat', 5),
 ('gniavei', 'Cat', 5),
-('aachesonj', 'Cat', 5);
+('aachesonj', 'Cat', 5),
+('sollicitudin', 'Cat', 10),
+('sollicitudin', 'Dog', 10),
+('accumsan', 'Cat', 10),
+('lacinia', 'Cat', 10),
+('lacinia', 'Dog', 10),
+('dignissim', 'Cat', 15),
+('praesen', 'Cat', 15);
+
 
 INSERT INTO specify (pcs_admin_username, care_taker_username, ptype, price) VALUES 
 ('agloyensa', 'acallera', 'Dog', 5),
@@ -138,7 +186,15 @@ INSERT INTO specify (pcs_admin_username, care_taker_username, ptype, price) VALU
 ('chanfreyg', 'lloadwickg', 'Cat', 5),
 ('pbrahanh', 'jmaddysonh', 'Cat', 5),
 ('pgornari', 'gniavei', 'Cat', 5),
-('wrosemanc', 'aachesonj', 'Cat', 5);
+('wrosemanc', 'aachesonj', 'Cat', 5),
+('ornare', 'sollicitudin', 'Cat', 10),
+('ornare', 'sollicitudin', 'Dog', 10),
+('ligula', 'accumsan', 'Cat', 10),
+('sociosqu', 'lacinia', 'Cat', 10),
+('sociosqu', 'lacinia', 'Dog', 10),
+('blandit', 'dignissim', 'Cat', 15),
+('facilisis','praesen', 'Cat', 15);
+
 
 INSERT INTO has_availability (care_taker_username, s_date, s_time, e_time) VALUES
 ('acallera', '2020-11-07', '08:00', '20:00'),
@@ -343,7 +399,22 @@ INSERT INTO has_availability (care_taker_username, s_date, s_time, e_time) VALUE
 ('aachesonj', '2020-11-18', '08:00', '10:00'),
 ('aachesonj', '2020-11-19', '10:00', '12:00'),
 ('aachesonj', '2020-11-20', '08:00', '10:00'),
-('aachesonj', '2020-11-21', '18:00', '20:00');
+('aachesonj', '2020-11-21', '18:00', '20:00'),
+('sollicitudin', '2020-11-07', '08:00', '20:00'),
+('sollicitudin', '2020-11-08', '08:00', '20:00'),
+('sollicitudin', '2020-11-09', '08:00', '20:00'),
+('sollicitudin', '2020-11-10', '08:00', '20:00'),
+('sollicitudin', '2020-11-11', '08:00', '20:00'),
+('sollicitudin', '2020-11-12', '08:00', '20:00'),
+('sollicitudin', '2020-11-13', '08:00', '20:00'),
+('sollicitudin', '2020-11-14', '08:00', '20:00'),
+('sollicitudin', '2020-11-15', '08:00', '20:00'),
+('sollicitudin', '2020-11-16', '08:00', '20:00'),
+('sollicitudin', '2020-11-17', '08:00', '20:00'),
+('sollicitudin', '2020-11-18', '08:00', '20:00'),
+('sollicitudin', '2020-11-19', '08:00', '20:00'),
+('sollicitudin', '2020-11-20', '08:00', '20:00');
+
 
 INSERT INTO does_service (care_taker_username, svc_type) VALUES 
 ('acallera', 'Pet Grooming'),
@@ -365,7 +436,15 @@ INSERT INTO does_service (care_taker_username, svc_type) VALUES
 ('lloadwickg', 'Full Day Pet Care'),
 ('jmaddysonh', 'Full Day Pet Care'),
 ('gniavei', 'Full Day Pet Care'),
-('aachesonj', 'Full Day Pet Care');
+('aachesonj', 'Full Day Pet Care'),
+('sollicitudin', 'Pet Grooming'),
+('sollicitudin', 'Full Day Pet Care'),
+('accumsan', 'Pet Grooming'),
+('lacinia', 'Pet Grooming'),
+('dignissim', 'Pet Grooming'),
+('dignissim', 'Full Day Pet Care'),
+('praesen', 'Pet Grooming');
+
 
 /* TO BE CONFIRMED */
 INSERT INTO bid (care_taker_username, s_date, s_time, e_time, name, pet_owner_username, review, price, trf_mthd, pay_type, rating, successful, svc_type) VALUES
