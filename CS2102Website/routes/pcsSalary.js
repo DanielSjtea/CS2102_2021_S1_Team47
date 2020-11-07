58b1188ac49434ce3e6fc01040edb27b3e9e8583
Aerin <aerinnnnn@gmail.com>
2020-11-07 13:19:45 +0800

    edited pcsSalary and pcsStatistics

--- CS2102Website/routes/pcsSalary.js
+++ CS2102Website/routes/pcsSalary.js
@@ -8,17 +8,21 @@ const { get_fulltime_self_salary_month } = require("../data/queries");
 
 
 function getMonthFromString(mon){
-    return new Date(Date.parse(mon +" 1, 2012")).getMonth()+1
+    return new Date(Date.parse(mon +" 1, 2012")).getMonth()+1;
 }
 
-router.get("/", function(req, res, next) {
-    
+router.get("/", async function(req, res, next) {
+    var today = new Date();
+    var date = today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.toString().substring(8,10);
+    let totalSalaryParams = [date];
+    var totalSalary = await database.db_get_promise(sql.get_total_salary_month, totalSalaryParams);
     res.render("pcsSalary", {
         ct_name: '-',
         username: 'Please select a user to display',
         jobType: '-',
         salary: '-',
-        bonus: '-'
+        bonus: '-',
+        totalSalary: totalSalary
     });
 });
 
@@ -26,7 +30,7 @@ router.get("/", function(req, res, next) {
 router.post("/", async function(req, res, next) {
     var month = req.body.month;
     var year = req.body.year;
-    var input_name = req.body.input_name;
+    var input_name = req.body.name;
     var username = req.body.username;
 
     database.query(sql.is_caretaker, [username], async (err, data) => {
@@ -35,30 +39,32 @@ router.post("/", async function(req, res, next) {
             const invalid = "Invalid username of caretaker!";
             res.redirect("pcsSalary");
         } else {
-            var caretaker = await database.db_get_promise(sql.get_caretaker_profile, [username]);
-            var ct_name = caretaker[0].name;
-            var ctype;
-            var date;
-            if (input_name == ct_name) {
-                ctype = caretaker[0].ctype;
-                date = [year, getMonthFromString(month), '28'].join('-');
+            if (data.rowCount > 0) {      
+                var caretaker = await database.db_get_promise(sql.get_caretaker_profile, [username]);
+                var ct_name = caretaker[0].name;
+                var ctype = caretaker[0].ctype;
+                var date = [year, getMonthFromString(month), '28'].join('-');
+                var salary;
                 if (ctype.toString() == "Full Time") {
                     let params = [username, date];
-                    var salary = await database.db_get_promise(sql.get_fulltime_self_salary_month, params);
+                    salary = await database.db_get_promise(sql.get_fulltime_self_salary_month, params);
                 } else {
                     let params = [username, date];
-                    var salary = await database.db_get_promise(sql.get_parttime_self_salary_month, params);
+                    salary = await database.db_get_promise(sql.get_parttime_self_salary_month, params);
                 }
-                //let totalSalaryParams = [date, username];
-                //var totalSalary = await database.db_get_promise(sql.get_totol_salary_month, totalSalaryParams);
+                var bonus = await database.db_get_promise(sql.get_bonus, [username, getMonthFromString(month)]);
+                var today = new Date();
+                var dateToday = today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.toString().substring(8,10);
+                let totalSalaryParams = [dateToday];
+                var totalSalary = await database.db_get_promise(sql.get_total_salary_month, totalSalaryParams);
                 res.render("pcsSalary", { 
                     username: username,
                     ct_name: ct_name,
                     jobType: ctype,
-                    salary: salary[0].pay,
-                    //bonus: salary[0].bonus
-                });
-                
+                    salary: salary,
+                    bonus: bonus,
+                    totalSalary: totalSalary
+                }); 
             } else {
                 res.redirect("pcsSalary");
             }
