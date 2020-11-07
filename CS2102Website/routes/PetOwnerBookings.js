68a51619d1e96a2d3029a4c5c0b91ff2f5ea1a83
Aerin <aerinnnnn@gmail.com>
2020-11-07 16:16:12 +0800

    edited petownerbookings

--- CS2102Website/routes/PetOwnerBookings.js
+++ CS2102Website/routes/PetOwnerBookings.js
@@ -13,36 +13,38 @@ router.get("/", function(req, res, next) {
         } else {
             var bidArr = new Array(); // array to store dictionary of things
             if(podata.rowCount > 0) {
-            var dict = {};
-            for (var i = 0; i < podata.rowCount; i++) {
-                var ctUsername = podata.rows[i].care_taker_username;
-//                var sDate = podata.rows[i].s_date;
-                dict["pType"] = new Array();
-                dict["ctUsername"] = podata.rows[i].care_taker_username;
-                dict["sDate"] = podata.rows[i].s_date;
-             };
+                var dict = {};
+                for (var i = 0; i < podata.rowCount; i++) {
+                    var ctUsername = podata.rows[i].care_taker_username;
+    //                var sDate = podata.rows[i].s_date;
+                    dict["pType"] = new Array();
+                    dict["ctUsername"] = podata.rows[i].care_taker_username;
+                    dict["sDate"] = podata.rows[i].s_date;
+                };
 
-             //getting name of username
-             for (var i = 0; i < podata.rowCount; i++) {
-                database.query(sql.get_profile, [podata.rows[i].care_taker_username], (err, profdata) => {
-                   dict["ctName"] = profdata.rows[0].name;
-                   dict["contact"] = profdata.rows[0].contact_num;
-                });
-             };
-              // getting petType of username
-             for (var i = 0; i < podata.rowCount; i++) {
-                database.query(sql.get_ct_pet_types, [podata.rows[i].care_taker_username], (err, pdata) => {
-                   //var pType = pdata.rows[0].ptype;
-                   for (var i = 0; i < podata.rowCount; i++) {
-                       dict["pType"].push(pdata.rows[i].ptype); // pet types
-                   }
-                   bidArr.push(dict);
-                   console.log("stringify " + JSON.stringify(bidArr));
-                    res.render("PetOwnerBookings",{
-                        bidArr:bidArr
+                //getting name of username
+                for (var i = 0; i < podata.rowCount; i++) {
+                    database.query(sql.get_profile, [podata.rows[i].care_taker_username], (err, profdata) => {
+                    dict["ctName"] = profdata.rows[0].name;
+                    dict["contact"] = profdata.rows[0].contact_num;
                     });
+                };
+                // getting petType of username
+                for (var i = 0; i < podata.rowCount; i++) {
+                    database.query(sql.get_ct_pet_types, [podata.rows[i].care_taker_username], (err, pdata) => {
+                    //var pType = pdata.rows[0].ptype;
+                    for (var i = 0; i < podata.rowCount; i++) {
+                        if (pdata.rows[i] != null) {
+                            dict["pType"].push(pdata.rows[i].ptype); // pet types
+                        }
+                    }
+                    bidArr.push(dict);
+                    //console.log("stringify " + JSON.stringify(bidArr));   
+                    });
+                };
+                res.render("PetOwnerBookings",{
+                    bidArr:bidArr
                 });
-             };
              }else {
                 res.render("PetOwnerBookings", {
                     bidArr: null
@@ -51,5 +53,11 @@ router.get("/", function(req, res, next) {
         }
     });
 });
+
+router.post("/viewProfile", function(req, res, next) {
+    var ctUser = req.body.careTakerUser;
+    req.session.caretakerUsername = ctUser;
+    res.redirect("viewSitterProfile");
+})
 
 module.exports = router;
